77a0e3237c09e9231133c11deabb3ef31bf98c7b
Yves NDJ <yndjamba@live.fr>
2020-11-19 08:40:46 +0100

    maj price

--- src/components/Burger/BuilderControls/BuildControls.js
+++ src/components/Burger/BuilderControls/BuildControls.js
@@ -13,6 +13,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.buildControls}>
+        <p>Current Price : <strong>{props.price.toFixed(2)} $ </strong></p>
         {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
@@ -22,6 +23,7 @@ const buildControls = (props) => (
                 disabled={props.disabled[ctrl.type]}
             />
         ))}
+        <button>ORDER NOW</button>
     </div>
 );
 
